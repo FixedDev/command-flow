a3cdc6f6dd28b604c87429ec2b01e6b3e101c092
FixedDev <gilbertodamian14@gmail.com>
2020-08-31 17:48:55 -0500

    Added an exception to denote when the CommandUsage must be send to the sender

---
+++ Universal/src/main/java/me/fixeddev/commandflow/exception/CommandUsage.java
@@ -0,0 +1,29 @@
+package me.fixeddev.commandflow.exception;
+
+import net.kyori.text.Component;
+
+public class CommandUsage extends CommandException {
+    public CommandUsage() {
+    }
+
+    public CommandUsage(Component message) {
+        super(message);
+    }
+
+    public CommandUsage(String message) {
+        super(message);
+    }
+
+    public CommandUsage(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommandUsage(Throwable cause) {
+        super(cause);
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+}
