bf8f25e2c87df5278e8fd6f5d4fec03412d32181
FixedDev <gilbertodamian14@gmail.com>
2020-09-05 01:38:31 -0500

    Added throws CommandException for the execute

--- Universal/src/main/java/me/fixeddev/commandflow/command/Action.java
+++ Universal/src/main/java/me/fixeddev/commandflow/command/Action.java
@@ -1,9 +1,10 @@
 package me.fixeddev.commandflow.command;
 
 import me.fixeddev.commandflow.CommandContext;
+import me.fixeddev.commandflow.exception.CommandException;
 
 public interface Action {
     static Action NULL_ACTION = context -> false;
 
-    boolean execute(CommandContext context);
+    boolean execute(CommandContext context) throws CommandException;
 }
