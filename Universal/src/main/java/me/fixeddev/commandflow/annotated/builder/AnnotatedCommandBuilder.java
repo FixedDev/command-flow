1d37dcc3f976105b183e2d7eadd0c91fd610ef98
FixedDev <gilbertodamian14@gmail.com>
2020-09-06 00:22:24 -0500

    Added a create static method

--- Universal/src/main/java/me/fixeddev/commandflow/annotated/builder/AnnotatedCommandBuilder.java
+++ Universal/src/main/java/me/fixeddev/commandflow/annotated/builder/AnnotatedCommandBuilder.java
@@ -1,5 +1,11 @@
 package me.fixeddev.commandflow.annotated.builder;
 
+import me.fixeddev.commandflow.annotated.part.PartInjector;
+
 public interface AnnotatedCommandBuilder {
     CommandDataNode newCommand(String name);
+
+    static AnnotatedCommandBuilder create(PartInjector injector) {
+        return new AnnotatedCommandBuilderImpl(injector);
+    }
 }
