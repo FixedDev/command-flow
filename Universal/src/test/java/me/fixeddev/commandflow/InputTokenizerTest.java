84cbcd1cd6b42f4fdc4eab1ed62366515d97f181
yusshu <yushu@unnamed.team>
2020-09-08 23:16:59 -0500

    Fix test. Don't print the tokens, compare it with JUnit's assertions

--- Universal/src/test/java/me/fixeddev/commandflow/InputTokenizerTest.java
+++ Universal/src/test/java/me/fixeddev/commandflow/InputTokenizerTest.java
@@ -2,6 +2,7 @@ package me.fixeddev.commandflow;
 
 import me.fixeddev.commandflow.input.InputTokenizer;
 import me.fixeddev.commandflow.input.QuotedSpaceTokenizer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,9 +17,14 @@ public class InputTokenizerTest {
                 "command subcommand \"single ' quote\" 'double \" quote' other 'nonclosed non\"closed"
         );
 
-        for (String token : tokens) {
-            System.out.println(token);
-        }
+        Assertions.assertEquals(7, tokens.size());
+        Assertions.assertEquals("command", tokens.get(0));
+        Assertions.assertEquals("subcommand", tokens.get(1));
+        Assertions.assertEquals("single ' quote", tokens.get(2));
+        Assertions.assertEquals("double \" quote", tokens.get(3));
+        Assertions.assertEquals("other", tokens.get(4));
+        Assertions.assertEquals("'nonclosed", tokens.get(5));
+        Assertions.assertEquals("non\"closed", tokens.get(6));
 
     }
 
