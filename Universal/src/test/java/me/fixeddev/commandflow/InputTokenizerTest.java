2ed5fc4dbab6961357cb745eb919c219f0abce9a
yusshu <yushu@unnamed.team>
2020-09-08 23:12:57 -0500

    Make quoted space tokenizer work. Add test for InputTokenizer

--- Universal/src/test/java/me/fixeddev/commandflow/InputTokenizerTest.java
+++ Universal/src/test/java/me/fixeddev/commandflow/InputTokenizerTest.java
@@ -1,4 +1,25 @@
 package me.fixeddev.commandflow;
 
+import me.fixeddev.commandflow.input.InputTokenizer;
+import me.fixeddev.commandflow.input.QuotedSpaceTokenizer;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class InputTokenizerTest {
+
+    @Test
+    public void testQuotedSpaceTokenizer() {
+
+        InputTokenizer tokenizer = new QuotedSpaceTokenizer();
+        List<String> tokens = tokenizer.tokenize(
+                "command subcommand \"single ' quote\" 'double \" quote' other 'nonclosed non\"closed"
+        );
+
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+
+    }
+
 }
