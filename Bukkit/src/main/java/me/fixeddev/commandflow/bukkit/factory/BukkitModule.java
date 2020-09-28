7d0d287ab56f61258897982868d2569610c3bf14
FixedDev <gilbertodamian14@gmail.com>
2020-09-28 08:28:32 -0500

    Created the @Sender annotation and created the BukkitModule for the PartInjector

---
+++ Bukkit/src/main/java/me/fixeddev/commandflow/bukkit/factory/BukkitModule.java
@@ -0,0 +1,18 @@
+package me.fixeddev.commandflow.bukkit.factory;
+
+import me.fixeddev.commandflow.annotated.part.AbstractModule;
+import me.fixeddev.commandflow.annotated.part.Key;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class BukkitModule extends AbstractModule {
+    @Override
+    public void configure() {
+        bindFactory(CommandSender.class, new CommandSenderFactory());
+        bindFactory(OfflinePlayer.class, new OfflinePlayerPartFactory());
+        bindFactory(Player.class, new PlayerSenderFactory());
+        bindFactory(new Key(Player.class, Sender.class), new PlayerSenderFactory());
+    }
+}
