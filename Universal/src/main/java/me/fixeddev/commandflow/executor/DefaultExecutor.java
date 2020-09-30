3d560b5aa0db243d37f5494645adb7f6cf25bd96
FixedDev <gilbertodamian14@gmail.com>
2020-09-30 10:38:27 -0500

    Catch the NoMoreArgumentsException and instead throw the CommandUsage exception with a valid usage, that exception shouldn't be propagated after the CommandManager

--- Universal/src/main/java/me/fixeddev/commandflow/executor/DefaultExecutor.java
+++ Universal/src/main/java/me/fixeddev/commandflow/executor/DefaultExecutor.java
@@ -3,6 +3,7 @@ package me.fixeddev.commandflow.executor;
 import me.fixeddev.commandflow.CommandContext;
 import me.fixeddev.commandflow.command.Command;
 import me.fixeddev.commandflow.exception.CommandUsage;
+import me.fixeddev.commandflow.exception.NoMoreArgumentsException;
 import net.kyori.text.Component;
 import net.kyori.text.TextComponent;
 
@@ -12,23 +13,39 @@ public class DefaultExecutor implements Executor {
         Command toExecute = commandContext.getCommand();
 
         if (toExecute != null) {
-            if (!toExecute.getAction().execute(commandContext)) {
-                // TODO: send the message
-                String label = String.join(" ", commandContext.getLabels());
+            try {
+                if (!toExecute.getAction().execute(commandContext)) {
+                    CommandUsage usage = new CommandUsage(getUsage(commandContext));
+                    usage.setCommand(toExecute);
 
-                Component labelComponent = TextComponent.of(label);
-                Component partComponents = toExecute.getPart().getLineRepresentation();
-
-                if (partComponents != null) {
-                    labelComponent = labelComponent.append(TextComponent.of(" ")).append(partComponents);
+                    throw usage;
                 }
+            } catch (NoMoreArgumentsException e) {
+                CommandUsage usage = new CommandUsage(getUsage(commandContext));
+                usage.setCommand(toExecute);
 
-                throw new CommandUsage(labelComponent);
+                throw usage;
             }
+
         } else {
             return false;
         }
 
         return true;
+    }
+
+    private Component getUsage(CommandContext commandContext) {
+        Command toExecute = commandContext.getCommand();
+
+        String label = String.join(" ", commandContext.getLabels());
+
+        Component labelComponent = TextComponent.of(label);
+        Component partComponents = toExecute.getPart().getLineRepresentation();
+
+        if (partComponents != null) {
+            labelComponent = labelComponent.append(TextComponent.of(" ")).append(partComponents);
+        }
+
+        return labelComponent;
     }
 }
