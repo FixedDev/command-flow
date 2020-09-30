e993500820a58c163e3a172ab48daeee2185ebad
FixedDev <gilbertodamian14@gmail.com>
2020-09-30 11:02:41 -0500

    The Executor now gets an UsageBuilder instance passed on, also, don't catch the NoMoreArgumentsException there, it isn't expected so it should be an error

--- Universal/src/main/java/me/fixeddev/commandflow/executor/DefaultExecutor.java
+++ Universal/src/main/java/me/fixeddev/commandflow/executor/DefaultExecutor.java
@@ -4,48 +4,26 @@ import me.fixeddev.commandflow.CommandContext;
 import me.fixeddev.commandflow.command.Command;
 import me.fixeddev.commandflow.exception.CommandUsage;
 import me.fixeddev.commandflow.exception.NoMoreArgumentsException;
+import me.fixeddev.commandflow.usage.UsageBuilder;
 import net.kyori.text.Component;
 import net.kyori.text.TextComponent;
 
 public class DefaultExecutor implements Executor {
     @Override
-    public boolean execute(CommandContext commandContext) {
+    public boolean execute(CommandContext commandContext, UsageBuilder builder) {
         Command toExecute = commandContext.getCommand();
 
         if (toExecute != null) {
-            try {
-                if (!toExecute.getAction().execute(commandContext)) {
-                    CommandUsage usage = new CommandUsage(getUsage(commandContext));
-                    usage.setCommand(toExecute);
-
-                    throw usage;
-                }
-            } catch (NoMoreArgumentsException e) {
-                CommandUsage usage = new CommandUsage(getUsage(commandContext));
+            if (!toExecute.getAction().execute(commandContext)) {
+                CommandUsage usage = new CommandUsage(builder.getUsage(commandContext));
                 usage.setCommand(toExecute);
 
                 throw usage;
             }
-
         } else {
             return false;
         }
 
         return true;
-    }
-
-    private Component getUsage(CommandContext commandContext) {
-        Command toExecute = commandContext.getCommand();
-
-        String label = String.join(" ", commandContext.getLabels());
-
-        Component labelComponent = TextComponent.of(label);
-        Component partComponents = toExecute.getPart().getLineRepresentation();
-
-        if (partComponents != null) {
-            labelComponent = labelComponent.append(TextComponent.of(" ")).append(partComponents);
-        }
-
-        return labelComponent;
     }
 }
--- Universal/src/main/java/me/fixeddev/commandflow/executor/Executor.java
+++ Universal/src/main/java/me/fixeddev/commandflow/executor/Executor.java
@@ -2,7 +2,8 @@ package me.fixeddev.commandflow.executor;
 
 import me.fixeddev.commandflow.CommandContext;
 import me.fixeddev.commandflow.exception.CommandException;
+import me.fixeddev.commandflow.usage.UsageBuilder;
 
 public interface Executor {
-    boolean execute(CommandContext commandContext) throws CommandException;
+    boolean execute(CommandContext commandContext, UsageBuilder builder) throws CommandException;
 }
