d67fe75730b30fdbb0cfe332af03c6e530d8d800
FixedDev <gilbertodamian14@gmail.com>
2020-10-01 16:34:32 -0500

    Add documentation for Authorizer

--- Universal/src/main/java/me/fixeddev/commandflow/Authorizer.java
+++ Universal/src/main/java/me/fixeddev/commandflow/Authorizer.java
@@ -1,5 +1,12 @@
 package me.fixeddev.commandflow;
 
 public interface Authorizer {
+    /**
+     * If an executor has certain permission, meaning that it is authorized.
+     *
+     * @param namespace  The {@link Namespace} that contains injected values like the source.
+     * @param permission The permission to check against.
+     * @return If the source is authorized to this permission, true if the permission is empty.
+     */
     boolean isAuthorized(Namespace namespace, String permission);
 }
