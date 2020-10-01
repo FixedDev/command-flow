79e2cc757a77fb7c8a2a61814af3caf073aed9ed
FixedDev <gilbertodamian14@gmail.com>
2020-10-01 16:42:49 -0500

    Add documentation for the CommandClass and SubCommandInstanceCreator

--- Universal/src/main/java/me/fixeddev/commandflow/annotated/CommandClass.java
+++ Universal/src/main/java/me/fixeddev/commandflow/annotated/CommandClass.java
@@ -1,4 +1,7 @@
 package me.fixeddev.commandflow.annotated;
 
+/**
+ * A marker interface for Commands based on annotations.
+ */
 public interface CommandClass {
 }
--- Universal/src/main/java/me/fixeddev/commandflow/annotated/SubCommandInstanceCreator.java
+++ Universal/src/main/java/me/fixeddev/commandflow/annotated/SubCommandInstanceCreator.java
@@ -1,5 +1,16 @@
 package me.fixeddev.commandflow.annotated;
 
+/**
+ * An interface with the purpose of creating a {@link CommandClass} instance that is a sub commands class of another
+ * {@link CommandClass}, with that class being the parent of this one.
+ */
 public interface SubCommandInstanceCreator {
+    /**
+     * Create an instance of an specified {@link CommandClass} with a given {@link CommandClass} as parent.
+     *
+     * @param clazz  The type of the {@link CommandClass} to create an instance of.
+     * @param parent The parent instance of this {@link CommandClass}.
+     * @return A {@link CommandClass} instance.
+     */
     CommandClass createInstance(Class<? extends CommandClass> clazz, CommandClass parent);
 }
