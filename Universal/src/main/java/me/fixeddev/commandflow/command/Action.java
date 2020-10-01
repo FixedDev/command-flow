94b7a92d48e2acc0d9bfb524dbd3e4679d2b8bc1
FixedDev <gilbertodamian14@gmail.com>
2020-10-01 16:28:23 -0500

    Add documentation to the Action class

--- Universal/src/main/java/me/fixeddev/commandflow/command/Action.java
+++ Universal/src/main/java/me/fixeddev/commandflow/command/Action.java
@@ -4,7 +4,17 @@ import me.fixeddev.commandflow.CommandContext;
 import me.fixeddev.commandflow.exception.CommandException;
 
 public interface Action {
+    /**
+     * A NOP default Action used for {@link Command} instances which action isn't defined
+     */
     static Action NULL_ACTION = context -> false;
 
+    /**
+     * The action to execute when the {@link Command} is being called/run by the {@link me.fixeddev.commandflow.CommandManager}.
+     *
+     * @param context The {@link CommmandContext} for this executed command.
+     * @return If the action was executed sucessfully or not, if the value is false, the usage will be send to the source.
+     * @throws CommandException If an error occurs while executing the action.
+     */
     boolean execute(CommandContext context) throws CommandException;
 }
