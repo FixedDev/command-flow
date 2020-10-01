9653e728f5ad64d2b7236d606ef519122abee360
FixedDev <gilbertodamian14@gmail.com>
2020-10-01 17:27:41 -0500

    Modify how the documentation is presented

--- Universal/src/main/java/me/fixeddev/commandflow/annotated/AnnotatedCommandTreeBuilder.java
+++ Universal/src/main/java/me/fixeddev/commandflow/annotated/AnnotatedCommandTreeBuilder.java
@@ -15,20 +15,22 @@ public interface AnnotatedCommandTreeBuilder {
      * Creates a {@linkplain Command} tree from the given {@link CommandClass} instance.
      * The {@link Command} instances created from annotated methods should be public instance methods(not static ones) that return boolean or void,
      * not being inherited from other classes.
-     *
-     * @param commandClass The {@link CommandClass} instance of this command tree.
-     * @return A not null list of of {@link Command} containing the main level commands which are on this class.
      * <p>
-     * @see me.fixeddev.commandflow.annotated.annotation.Command It is used to indicate whether a method or a class is a Command,
+     * The {@link me.fixeddev.commandflow.annotated.annotation.Command} annotation is used to indicate whether a method or a class is a Command,
      * if a class has this annotation, that means that every command in that class is his subcommand, with the main command being
      * a command with empty name.
-     * @see me.fixeddev.commandflow.annotated.annotation.Handler It is used to indicate that a method of a class with subcommands is the one that
+     * <p>
+     * The {@link me.fixeddev.commandflow.annotated.annotation.Handler} annotation is used to indicate that a method of a class with subcommands is the one that
      * handles the sub commands, actualy being a {@link me.fixeddev.commandflow.part.defaults.SubCommandPart.SubCommandHandler} but created with
      * an annotated method.
-     * @see me.fixeddev.commandflow.annotated.annotation.SubCommandClasses This annotation is used to indicate that the given classes are actually subcommands
+     * <p>
+     * The {@link me.fixeddev.commandflow.annotated.annotation.SubCommandClasses} annotation is  used to indicate that the given classes are actually subcommands
      * of the given {@link CommandClass}, these sub command classes will be instantiated using a {@link SubCommandInstanceCreator} and
      * parsed with this same method, the returned commands will be added as subcommands for the provided {@link CommandClass}.
      * This allows multiple levels of subcommands without a lot of hassle.
+     *
+     * @param commandClass The {@link CommandClass} instance of this command tree.
+     * @return A not null list of of {@link Command} containing the main level commands which are on this class.
      */
     List<Command> fromClass(CommandClass commandClass);
 
