22edb33d118bd50e21696b58ee64e31e343098a4
OcZi <atlas200x@gmail.com>
2020-10-12 02:47:54 -0500

    Create World Part & Factory - Added to BukkitModule

--- Bukkit/src/main/java/me/fixeddev/commandflow/bukkit/factory/BukkitModule.java
+++ Bukkit/src/main/java/me/fixeddev/commandflow/bukkit/factory/BukkitModule.java
@@ -4,6 +4,7 @@ import me.fixeddev.commandflow.annotated.part.AbstractModule;
 import me.fixeddev.commandflow.annotated.part.Key;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -13,6 +14,7 @@ public class BukkitModule extends AbstractModule {
         bindFactory(CommandSender.class, new CommandSenderFactory());
         bindFactory(OfflinePlayer.class, new OfflinePlayerPartFactory());
         bindFactory(Player.class, new PlayerPartFactory());
+        bindFactory(World.class, new WorldFactory());
         bindFactory(new Key(Player.class, Sender.class), new PlayerSenderFactory());
     }
 }
