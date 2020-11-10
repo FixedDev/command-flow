a8d72b33b320ed7726106ba64718a2c2cd5a8679
FixedDev <gilbertodamian14@gmail.com>
2020-11-09 20:14:47 -0600

    The optional parts don't have suggestions

--- Universal/src/main/java/me/fixeddev/commandflow/part/defaults/OptionalPart.java
+++ Universal/src/main/java/me/fixeddev/commandflow/part/defaults/OptionalPart.java
@@ -14,6 +14,7 @@ import net.kyori.text.TextComponent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OptionalPart implements CommandPart, SinglePartWrapper {
@@ -76,7 +77,7 @@ public class OptionalPart implements CommandPart, SinglePartWrapper {
 
     @Override
     public List<String> getSuggestions(CommandContext commandContext, ArgumentStack stack) {
-        return part.getSuggestions(commandContext, stack);
+        return Collections.emptyList();
     }
 
     @Override
