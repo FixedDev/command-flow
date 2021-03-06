56d9e71aa90a40581e1e8ecd719e63327c840504
FixedDev <gilbertodamian14@gmail.com>
2021-03-06 00:24:00 -0600

    Since Yushu is retarded, the translations weren't added yet

--- Universal/src/main/java/me/fixeddev/commandflow/translator/DefaultMapTranslationProvider.java
+++ Universal/src/main/java/me/fixeddev/commandflow/translator/DefaultMapTranslationProvider.java
@@ -15,6 +15,11 @@ public class DefaultMapTranslationProvider implements TranslationProvider{
         translations.put("command.no-permission", "No permission.");
         translations.put("argument.no-more","No more arguments were found, size: %s position: %s");
         translations.put("number.out-range", "The number %s is not within the range min: %s max: %s");
+        translations.put("invalid.byte", "The number %s is not a valid byte!");
+        translations.put("invalid.integer", "The number %s is not a valid integer!");
+        translations.put("invalid.float", "The number %s is not a valid float!");
+        translations.put("invalid.double", "The number %s is not a valid double!");
+        translations.put("invalid.boolean", "The string %s is not a valid boolean!");
     }
 
     public String getTranslation(String key) {
