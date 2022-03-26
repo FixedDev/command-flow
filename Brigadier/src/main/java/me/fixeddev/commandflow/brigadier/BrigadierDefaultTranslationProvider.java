b124ca9f68b5de51ccd9a8d72cf39b6576496f54
FixedDev <gilbertodamian14@gmail.com>
2022-03-26 15:06:31 -0600

    fix?: Changed translation for parse error.

--- Brigadier/src/main/java/me/fixeddev/commandflow/brigadier/BrigadierDefaultTranslationProvider.java
+++ Brigadier/src/main/java/me/fixeddev/commandflow/brigadier/BrigadierDefaultTranslationProvider.java
@@ -4,7 +4,7 @@ import me.fixeddev.commandflow.bukkit.BukkitDefaultTranslationProvider;
 
 public class BrigadierDefaultTranslationProvider extends BukkitDefaultTranslationProvider {
     public BrigadierDefaultTranslationProvider() {
-        translations.put("selector.parse-error", "Failed to parse selector: %s for %s");
+        translations.put("selector.parse-error", "Failed to parse selector %s:\n %s");
         translations.put("selector.ambiguous", "The selector %s is ambiguous (more than one player matched), leaving it as is.");
     }
 }
