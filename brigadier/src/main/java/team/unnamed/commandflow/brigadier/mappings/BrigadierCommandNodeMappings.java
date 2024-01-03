0bcad4f25dddac150cec9246e778f776baca282a
Damian G <gilbertodamian14@gmail.com>
2024-01-02 21:52:53 -0600

    docs: Add javadocs for every new class.

--- brigadier/src/main/java/team/unnamed/commandflow/brigadier/mappings/BrigadierCommandNodeMappings.java
+++ brigadier/src/main/java/team/unnamed/commandflow/brigadier/mappings/BrigadierCommandNodeMappings.java
@@ -4,12 +4,29 @@ import java.lang.reflect.Type;
 import java.util.Optional;
 
 public interface BrigadierCommandNodeMappings<T> {
+    /**
+     * Obtains the {@link CommandNodeMapping} for the specified type.
+     *
+     * @param type The type to get a conversion mapping for.
+     * @return A {@link CommandNodeMapping}, if it exists, suitable for converting a {@linkplain team.unnamed.commandflow.part.ArgumentPart} of the specified type.
+     */
     default Optional<CommandNodeMapping<T>> getMapping(Class<?> type) {
         return getMapping(type);
     }
 
+    /**
+     * Obtains the {@link CommandNodeMapping} for the specified type.
+     *
+     * @param type The type to get a conversion mapping for.
+     * @return A {@link CommandNodeMapping}, if it exists, suitable for converting a {@linkplain team.unnamed.commandflow.part.ArgumentPart} of the specified type.
+     */
     Optional<CommandNodeMapping<T>> getMapping(Type type);
 
+    /**
+     * Converts this Mappings instance into a builder, allowing for more mappings to be added.
+     *
+     * @return A new builder for a {@linkplain BrigadierCommandNodeMappings} instance, with the mappings of this instance already added.
+     */
     Builder<T> toBuilder();
 
     static <T> Builder<T> builder() {
