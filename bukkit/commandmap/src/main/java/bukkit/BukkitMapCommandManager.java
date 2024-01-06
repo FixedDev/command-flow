150d65d0cfdc8d15fe84db4d908236592cf3526d
Damian G <gilbertodamian14@gmail.com>
2024-01-06 14:56:10 -0600

    fix: Add support for PluginIdentifiableCommands.

--- bukkit/commandmap/src/main/java/bukkit/BukkitMapCommandManager.java
+++ bukkit/commandmap/src/main/java/bukkit/BukkitMapCommandManager.java
@@ -1,5 +1,7 @@
 package bukkit;
 
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 import team.unnamed.commandflow.CommandManager;
 import team.unnamed.commandflow.SimpleCommandManager;
 import team.unnamed.commandflow.bukkit.BukkitAuthorizer;
@@ -18,11 +20,23 @@ import java.util.logging.Level;
 public class BukkitMapCommandManager extends BukkitCommandManager {
 
     protected CommandMap bukkitCommandMap;
+    @NotNull
     protected final String fallbackPrefix;
+
+    protected Plugin plugin;
+
+    protected boolean usePluginCommands = false;
 
     protected final Map<String, BukkitCommandWrapper> wrapperMap;
 
-    public BukkitMapCommandManager(CommandManager delegate, String fallbackPrefix) {
+    public BukkitMapCommandManager(CommandManager delegate, Plugin plugin) {
+        this(delegate, plugin.getName());
+
+        this.plugin = plugin;
+        usePluginCommands = true;
+    }
+
+    public BukkitMapCommandManager(CommandManager delegate, @NotNull String fallbackPrefix) {
         super(delegate);
         this.fallbackPrefix = fallbackPrefix;
         wrapperMap = new HashMap<>();
@@ -35,8 +49,14 @@ public class BukkitMapCommandManager extends BukkitCommandManager {
         } catch (NoSuchFieldException | IllegalAccessException ex) {
             Bukkit.getLogger().log(Level.SEVERE, "Failed to get command map: ", ex);
         }
-
     }
+
+    public BukkitMapCommandManager(Plugin plugin) {
+        this(plugin.getName());
+        this.plugin = plugin;
+        this.usePluginCommands = true;
+    }
+
 
     public BukkitMapCommandManager(String fallbackPrefix) {
         this(new SimpleCommandManager(), fallbackPrefix);
@@ -48,8 +68,9 @@ public class BukkitMapCommandManager extends BukkitCommandManager {
 
     @Override
     protected void _register(Command command) {
-        BukkitCommandWrapper bukkitCommand = new BukkitCommandWrapper(command,
-                this, getTranslator());
+        BukkitCommandWrapper bukkitCommand = usePluginCommands ?
+                new PluginBukkitCommandWrapper(command, this, getTranslator(), plugin) :
+                new BukkitCommandWrapper(command, this, getTranslator());
 
         for (String alias : command.getAliases()) {
             registerCommand(fallbackPrefix + ":" + alias, command);
---
+++ bukkit/commandmap/src/main/java/bukkit/PluginBukkitCommandWrapper.java
@@ -0,0 +1,26 @@
+package bukkit;
+
+import org.bukkit.command.PluginIdentifiableCommand;
+import org.bukkit.plugin.Plugin;
+import team.unnamed.commandflow.bukkit.BukkitCommandManager;
+import team.unnamed.commandflow.command.Command;
+import team.unnamed.commandflow.translator.Translator;
+
+public class PluginBukkitCommandWrapper extends BukkitCommandWrapper implements PluginIdentifiableCommand {
+
+    private final Plugin plugin;
+
+    public PluginBukkitCommandWrapper(Command command,
+                                      BukkitCommandManager dispatcher,
+                                      Translator translator,
+                                      Plugin plugin) {
+        super(command, dispatcher, translator);
+
+        this.plugin = plugin;
+    }
+
+    @Override
+    public Plugin getPlugin() {
+        return plugin;
+    }
+}
